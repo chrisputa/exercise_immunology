ae34cab982db36965bcfdf79956dbfba1e687ac2
marcgab91 <marcgabriel@gmx.net>
2026-03-17 11:43:59 +0100

    Remove figures

--- src/content/figures/networks/dolphins/dolphins.ts
+++
@@ -1,4 +0,0 @@
-import dolphins from "./dolphins.json";
-export default dolphins;
-export const { graph, nodes, links } = dolphins;
-
--- src/content/figures/networks/sune/dtu.ts
+++
@@ -1,4 +0,0 @@
-import network from "./dtu.json";
-export default network;
-export const { nodes, links } = network;
-
